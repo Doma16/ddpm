09811cbf863978b36a63fff110c8d3af72ddf776
domaSaric <ds53234@fer.hr>
2023-07-26 17:52:38 +0200

    .

---
+++ ddpm.py
@@ -0,0 +1,119 @@
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+from torch import optim
+from .models.unet import UNet
+from utils import *
+
+import logging
+from torch.utils.tensorboard import SummarWriter
+
+logging.basicConfig(format="%(asctime)s - %(levelname)s: %(message)s", level=logging.INFO, datefmt="%I:%M:%S")
+
+class Diffusion:
+    def __init__(self, noise_steps=1000, beta_start=1e-4, beta_end=0.02, img_size=32, device='cpu'):
+        self.noise_steps = noise_steps
+        self.beta_start = beta_start
+        self.beta_end = beta_end
+
+        self.beta = self.prepare_noise_schedule().to(device)
+        self.alpha = 1. - self.beta
+        self.alpha_hat = torch.cumprod(self.alpha, dim=0)
+
+        self.img_size = img_size
+        self.device = device
+    
+    def prepare_noise_schedule(self):
+        return torch.linspace(self.beta_start, self.beta_end, self.noise_steps)
+    
+    def noise_images(self, x, t):
+        sqrt_alpha_hat = torch.sqrt(self.alpha_hat[t])[:, None, None, None]
+        sqrt_one_minus_alpha_hat = torch.sqrt(1 - self.alpha_hat[t])[:, None, None, None]
+        eps = torch.randn_like(x)
+        return sqrt_alpha_hat * x + sqrt_one_minus_alpha_hat * eps, eps
+    
+
+    def sample_timesteps(self, n):
+        return torch.randint(low=1, high=self.noise_steps, size=(n,))
+    
+    def sample(self, model, n, labels):
+        logging.info(f'Samping {n} new images...')
+        model.eval()
+        with torch.no_grad():
+            x = torch.randn((n, 3, self.img_size, self.img_size)).to(self.device)
+            for i in tqdm(reversed(range(1, self.noise_steps)), position=0):
+                t = (torch.ones(n) * i).long().to(self.device)
+                predicted_noise = model(x, t, labels)
+                
+                alpha =  self.alpha[t][:, None, None, None]
+                alpha_hat = self.alpha_hat[t][:, None, None, None]
+                beta = self.beta[t][:, None, None, None]
+                if i > 1:
+                    noise = torch.randn_like(x)
+                else:
+                    noise = torch.zeros_like(x)
+                
+                x = 1 / torch.sqrt(alpha) * (x - ((1 - alpha) / (torch.sqrt(1 - alpha_hat))) * predicted_noise) + torch.sqrt(beta) * noise
+        model.train()
+        x = (x.clamp(-1,1) + 1) / 2
+        x = (x * 255).type(torch.uint8)
+        return x
+    
+def train(args):
+    setup_logging(args.run_name)
+    device = args.device
+    dataloader = get_data(args)
+    model = UNet(num_classes=args.num_classes).to(device)
+    optimizer = optim.AdamW(model.parameters(), lr=args.lr)
+    mse = nn.MSELoss()
+    diffusion = Diffusion(img_size=args.image_size, device=device)
+    logger = SummarWriter(os.path.join('runs', args.run_name))
+    l = len(dataloader)
+    
+    for epoch in range(args.epochs):
+        logging.info(f'Starting epoch {epoch}:')
+        pbar = tqdm(dataloader)
+        for i, (images, labels) in enumerate(pbar):
+            images = images.to(device)
+            labels = labels.to(device)
+            t = diffusion.sample_timesteps(images.shapes[0]).to(device)
+            x_t, noise = diffusion.noise_images(images, t)
+            '''
+            if np.random.random() < 0.1:
+                labels = None
+            '''
+            predicted_noise = model(x_t, t, labels)
+            loss = mse(noise, predicted_noise)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            pbar.set_postfix(MSE=loss.item())
+            logger.add_scallar('MSE', loss.item(), global_step= epoch * l + 1)
+
+        if epoch % 10 == 0:
+            labels = torch.arange(10).long().to(device)
+            sampled_images = diffusion.sample(model, n=len(labels), labels=labels)
+            plot_images(sampled_images)
+            save_images(sampled_images, os.path.join('results', args.run_name, f'{epoch}.jpg'))
+            torch.save(model.state_dict(), os.path.join('models', args.run_name, f'ckpt.pt'))
+
+def launch():
+    import argparse
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    args.run_name = 'DDPM_c'
+    args.epochs = 300
+    args.batch_size = 14
+    args.image_size = 32
+    args.num_classes = 10
+    args.dataset_path = ''
+    args.device = 'cuda'
+    args.lr = 3e-4
+    train(args)
+
+if __name__ == '__main__':
+    launch()
